c1abb57ac4b42f8478694097478c82e0eabbf1b2
marinbaille-web <marin.baille@gmail.com>
2026-07-06 22:10:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const BABYBET_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyOifflbXPuCY13HSvRXvplyoXFydChx7p1pGJnfV6C56Z6aHcJIi6SCFBMbVkaoniE/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyX7qgg67QiqaeZNs1hrRdoyuqd0g3jTNWEIWlw7UoOJUOq1_Q9FR8IHLov_ZdW429I/exec"
 };
